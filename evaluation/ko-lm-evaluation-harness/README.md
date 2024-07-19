87d7ae6580edf8eb1a7a8592f574b4ec78fdc026
MIPIW <mipiw3842@snu.ac.kr>
2024-07-19 13:35:51 +0900

    ko-lm-evaluation_added

---
+++ evaluation/ko-lm-evaluation-harness/README.md
@@ -0,0 +1,38 @@
+# Ko LM Eval Harnesss
+
+- Ko LM eval harness에서 필요 없는 부분 지우고 다시 올리는 코드
+
+- evaluation for KOBEST, KLUE 등등 HAERAE 빼고 다
+- HAERAE는 evaluation2 폴더에서 돌리기
+
+1. run_multiple.sh에 포맷 잘 맞춰서 올리고, 
+2. rum_all.sh에 필요한 거 적당히 수정해서 돌리기.
+
+```
+@software{eval-harness,
+  author       = {Gao, Leo and
+                  Tow, Jonathan and
+                  Biderman, Stella and
+                  Black, Sid and
+                  DiPofi, Anthony and
+                  Foster, Charles and
+                  Golding, Laurence and
+                  Hsu, Jeffrey and
+                  McDonell, Kyle and
+                  Muennighoff, Niklas and
+                  Phang, Jason and
+                  Reynolds, Laria and
+                  Tang, Eric and
+                  Thite, Anish and
+                  Wang, Ben and
+                  Wang, Kevin and
+                  Zou, Andy},
+  title        = {A framework for few-shot language model evaluation},
+  month        = sep,
+  year         = 2021,
+  publisher    = {Zenodo},
+  version      = {v0.0.1},
+  doi          = {10.5281/zenodo.5371628},
+  url          = {https://doi.org/10.5281/zenodo.5371628}
+}
+```
---
+++ evaluation/ko-lm-evaluation-harness/run_multiple.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# watchout for the results are not be merged
+echo gemma is running
+sh run_all.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/google/gemma-2-9b-it/default/checkpoint-9000' '0,1,2,3' 'default' 'NULL' '1epoch'
+sh run_all.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/google/gemma-2-9b-it/lora/checkpoint-4500' '0,1,2,3' 'lora' 'google/gemma-2-9b-it' '1epoch'
+
+echo qwen is running
+sh run_all.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/Qwen/Qwen2-7B-Instruct/default/checkpoint-4500' '0,1,2,3' 'default' 'NULL' '1epoch'
+sh run_all.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/Qwen/Qwen2-7B-Instruct/lora/checkpoint-4500' '0,1,2,3' 'lora' 'Qwen/Qwen2-7B-Instruct' '1epoch'
+
+echo llama is running
+sh run_all.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/meta-llama/Meta-Llama-3-8B-Instruct/default/checkpoint-4500' '0,1,2,3' 'default' 'NULL' '1epoch'
+sh run_all.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/meta-llama/Meta-Llama-3-8B-Instruct/lora/checkpoint-4500' '0,1,2,3' 'lora' 'meta-llama/Meta-Llama-3-8B-Instruct' '1epoch'
