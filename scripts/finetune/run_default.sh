51b82c5768b1ce80252175b22e6925440098b12a
sildentraw <toscour345@snu.ac.kr>
2024-07-23 09:37:29 +0900

    Update run_default.sh

--- scripts/finetune/run_default.sh
+++ scripts/finetune/run_default.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo Qwen/Qwen2-7B-Instruct running
-python3 trainig_default.py \
+python3 training_default.py \
     --model Qwen/Qwen2-7B-Instruct \
     --train_batch_size 16 \
     --eval_batch_size 16 --epochs 1
 
 echo google/gemma-2-9b-it running
-python3 trainig_default.py \
+python3 training_default.py \
     --model google/gemma-2-9b-it \
     --train_batch_size 8 \
     --eval_batch_size 8 --epochs 1 
 
 echo meta-llama/Meta-Llama-3-16B-Instruct running
-python3 trainig_default.py \
+python3 training_default.py \
     --model meta-llama/Meta-Llama-3-8B-Instruct \
     --train_batch_size 16 \
     --eval_batch_size 16 --epochs 1 
