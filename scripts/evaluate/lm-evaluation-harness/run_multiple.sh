2186a3ec4a2adb4df87ff800b2ae1b0f89f391d7
MIPIW <mipiw3842@snu.ac.kr>
2024-08-02 10:51:51 +0900

    refine evaluation code

--- scripts/evaluate/lm-evaluation-harness/run_multiple.sh
+++ scripts/evaluate/lm-evaluation-harness/run_multiple.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/google/gemma-2-9b-it/default/checkpoint-9000' 'default' # '0,1,2,3'
-# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/google/gemma-2-9b-it/lora/checkpoint-4500' 'lora' 'google/gemma-2-9b-it' # '0,1,2,3'
-# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/Qwen/Qwen2-7B-Instruct/default/checkpoint-4500' 'default' # '0,1,2,3'
-# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/Qwen/Qwen2-7B-Instruct/lora/checkpoint-4500' 'lora' 'Qwen/Qwen2-7B-Instruct' # '0,1,2,3' 안돌아감
-# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/meta-llama/Meta-Llama-3-8B-Instruct/default/checkpoint-4500' 'default' # '0,1,2,3' not completed
-sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/meta-llama/Meta-Llama-3-8B-Instruct/lora/checkpoint-4500' 'lora' 'meta-llama/Meta-Llama-3-8B-Instruct' # '0,1,2,3' 안돌아감 
+# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/google/gemma-2-9b-it/default/checkpoint-9000' 'default' 
+# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/google/gemma-2-9b-it/lora/checkpoint-4500' 'lora' 'google/gemma-2-9b-it' 
+# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/Qwen/Qwen2-7B-Instruct/default/checkpoint-4500' 'default' 
+# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/Qwen/Qwen2-7B-Instruct/lora/checkpoint-4500' 'lora' 'Qwen/Qwen2-7B-Instruct' 
+# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/meta-llama/Meta-Llama-3-8B-Instruct/default/checkpoint-4500' 'default' 
+# sh run.sh '/home/hyohyeongjang/bigdata/ModelTrainer/llm-kr/outputs/checkpoints/meta-llama/Meta-Llama-3-8B-Instruct/lora/checkpoint-4500' 'lora' 'meta-llama/Meta-Llama-3-8B-Instruct' 
+
+# sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/qwen2-7b/dora/checkpoint-1065' 'lora' 'Qwen/Qwen2-7B-Instruct'
+# sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/llama3-8b/dora/checkpoint-1065' 'lora' 'meta-llama/Meta-Llama-3-8B-Instruct'
+sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/gemma2-9b/dora/checkpoint-1065' 'lora' 'google/gemma-2-9b-it' 
