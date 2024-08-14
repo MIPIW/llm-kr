43337078df7c1154f6f1ed6d14e31ffb5a8129ed
hyemijo <70941956+hyemijo@users.noreply.github.com>
2024-08-14 17:49:55 +0900

    update script for reformulating output with qwen2-7b-it

--- scripts/preproc/reformulate_output.py
+++ scripts/preproc/reformulate_output.py
@@ -1,5 +1,5 @@
 # pip install -U transformers
-
+import sys
 import os
 import random
 import numpy as np
@@ -14,7 +14,8 @@ from tqdm import tqdm
 from ast import literal_eval
 
 # 소요시간: 샘플 260개 & batch size 32-> 약 3분 (182.30 sec)
-# 커맨드 예시: python reformulate_output.py
+# eta: 약 10만 건 & batch size 8-> 100시간 (batch size 32: OOM)
+# 커맨드 예시: python reformulate_output.py data_it_240724_output_reformulate_1.csv 8 816  # row.Index 기준 816번 행부터 이어서 돌릴 때.
 
 
 # seed 설정 함수
@@ -67,7 +68,7 @@ class FormattingFunction:
         <|im_start|>system
         당신의 역할은 한국어로 답변하는 한국어 AI 어시트턴트입니다. 주어진 질문에 대해 한국어로 답변해주세요.\n<|im_end|>
         <|im_start|>user
-        아래 문장을 읽기 편하게 개행 문자와 볼드체를 활용하여 구조화된 형태로 바꿔주세요.\n\n{}<|im_end|>
+        아래 문장을 읽기 편하게 개행 문자, 글머리 기호, 볼드체를 활용하여 구조화된 형태로 바꿔주세요. 이때 말투는 공손한 존칭을 사용해주세요.\n\n{}<|im_end|>
         <|im_start|>system\n""",
         }
 
@@ -84,14 +85,20 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(device)
 
+    
+    if len(sys.argv) != 4:
+        sys.exit(f"python {sys.argv[0]} fname_input batch_size start_idx")
+        
+    F_INPUT_PATH = sys.argv[1] # F_INPUT_PATH = "data_it_240724_output_reformulate.csv" # "inference_sample_240731.csv"
+    BATCH_SIZE = int(sys.argv[2]) # 16
+    START_IDX = int(sys.argv[3])
     ############
     # constant 설정
     MODEL_PATH = "Qwen/Qwen2-7B-Instruct"
-    F_INPUT_PATH = "inference_sample_240731.csv"
     F_OUTPUT_PATH = F_INPUT_PATH[:-4] + "_output.tsv"
-    BATCH_SIZE = 32
+    MAX_LENGTH = 4096 # oom -> input truncation 추가
     ############
-
+    
     # 1. 모델 로드
     print("loading model & tokenizer...")
     model_path = MODEL_PATH
@@ -118,25 +125,38 @@ if __name__ == "__main__":
 
     # 3. 파일 i/o 준비
     # input (재구조화 전)
-    df = pd.read_csv(F_INPUT_PATH) 
+    df = pd.read_csv(F_INPUT_PATH)
+    tasks = df["task"]
     output_raw = df["output"]
 
     # output (재구조화 전후 비교)
     f_o = open(F_OUTPUT_PATH, "w", encoding="utf-8") 
-    header = "output\toutput_rvsd"
+    header = "index\toutput\toutput_rvsd"
     print(header, file=f_o)
 
     # 4. output 재구조화 (batch inference)
     _prompts = [formattingfunction(item) for item in output_raw ]
-    num_of_iteration = len(_prompts) // BATCH_SIZE
-
+    num_of_iteration = (len(_prompts) // BATCH_SIZE)
+    if len(_prompts) % BATCH_SIZE:
+        num_of_iteration += 1
+    
+    start_iter = START_IDX // BATCH_SIZE
     start_time = time.time()
     
     for i in tqdm(range(num_of_iteration)):
+        if i < start_iter:
+            continue
+            
         start = i * BATCH_SIZE
         _prompts_batch = _prompts[start : start + BATCH_SIZE] # batch size만큼 떼어오기
-        batched_inputs = tokenizer(_prompts_batch, padding=True, return_tensors="pt", return_token_type_ids=False).to(device)
+        
+        # truncation 적용: https://huggingface.co/docs/transformers/en/pad_truncation
+        # batched_inputs = tokenizer(_prompts_batch, padding=True, return_tensors="pt", return_token_type_ids=False, ).to(device)
+        # truncate & batch의 max length로 패딩
     
+        batched_inputs = tokenizer(_prompts_batch, padding=True, truncation=True, max_length=MAX_LENGTH, return_tensors="pt", return_token_type_ids=False).to(device)
+        
+        
         with torch.no_grad():
             # 언어모델로 inference
             outputs = model.generate(**batched_inputs, **gen_config)
@@ -149,8 +169,10 @@ if __name__ == "__main__":
                 generated_texts.append(generated_text)
             
             for idx, gen_text in enumerate(generated_texts):
-                input_text = df["output"][idx] # 재구조화 전
-                print( repr(input_text), repr(gen_text), sep="\t", file=f_o)
+                _idx = start + idx
+                _task = tasks[_idx]
+                input_text = output_raw[_idx] # 재구조화 전
+                print(_idx, _task, repr(input_text), repr(gen_text), sep="\t", file=f_o)
                 
     print(f"=========================== total generation took {time.time()-start_time:.2f} sec. ===========================")
     f_o.close()
