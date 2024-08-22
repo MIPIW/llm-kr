be0e391f60d8c1612a05b53b8214a8adcf4ca2ec
MIPIW <mipiw3842@snu.ac.kr>
2024-08-22 16:12:47 +0900

    add_evaluation_line

--- scripts/evaluate/lm-evaluation-harness/run_multiple.sh
+++ scripts/evaluate/lm-evaluation-harness/run_multiple.sh
@@ -14,6 +14,10 @@
 # sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/llama3.1-8b/dora/checkpoint-1065/' 'lora' 'meta-llama/Meta-Llama-3.1-8B-Instruct' 
 # sh run_all.sh 'meta-llama/Meta-Llama-3.1-8B-Instruct' 'default' 'X' 'base'
 
-sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/switch/gemma2-9b/dora/2of2/checkpoint-116' 'lora' 'google/gemma-2-9b-it'
-sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/switch/llama3.1-8b/dora/2of2/checkpoint-116' 'lora' 'meta-llama/Meta-Llama-3.1-8B-Instruct'
-sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/switch/llama3-8b/dora/2of2/checkpoint-116' 'lora' 'meta-llama/Meta-Llama-3-8B-Instruct'
+# sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/switch/gemma2-9b/dora/2of2/checkpoint-116' 'lora' 'google/gemma-2-9b-it'
+# sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/switch/llama3.1-8b/dora/2of2/checkpoint-116' 'lora' 'meta-llama/Meta-Llama-3.1-8B-Instruct'
+# sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/switch/llama3-8b/dora/2of2/checkpoint-116' 'lora' 'meta-llama/Meta-Llama-3-8B-Instruct'
+
+sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/switch/llama3.1-8b/no/2of2/checkpoint-116' 'default' 'X' '2of2'
+sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/switch/llama3-8b/no/2of2/checkpoint-116' 'default' 'X' '2of2'
+sh run_all.sh '/node_storage2/bigdata_IT_SFT/outputs/checkpoints/switch/qwen2-7b/dora/2of2/checkpoint-116' 'lora' 'Qwen/Qwen2-7B-Instruct' 
